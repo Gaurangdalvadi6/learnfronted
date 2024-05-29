49f5dfbe27b95a8848195829606ac946176cf4ce
gaurangdalvadi6 <gaurangdalvadi6@gmail.com>
2024-05-29 23:01:31 +0530

    javascript till logical operator

---
+++ JavaScript/app.js
@@ -0,0 +1,38 @@
+// console.log("Hello JavaScript");
+// console.log("Apni PathShala");
+// let a = 5;
+// let b = 10;
+// let output = `the Sum is ${a+b} .`;
+// console.log(output);
+// // console.log("Sum of a & b : ", a+b);
+// let age = 25;
+// console.log(age>18);
+// let x = "100";
+// console.log(typeof(x));
+// console.log(x);
+
+// let color = "green";
+// if (color == "red") {
+//     console.log("Stop! light color is red");
+// }
+// else if (color == "yellow") {
+//     console.log("slow down light color is yellow");
+// }
+// else if (color == "green") {
+//     console.log("go light color is green");
+// }
+// else{
+//     console.log("traffic light is broken");
+// }
+
+let size = "L";
+if (size == "XL") {
+    console.log("price is : 250");
+} else if (size == "L") {
+    console.log("price is : 200");
+}else if (size == "M") {
+    console.log("price is : 150");
+}
+else{
+    console.log("price is : 100");
+}
