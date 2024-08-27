0d9e46e5ea9585e7e9973973af4aa18459f4c7f3
gaurangdalvadi6 <gaurangdalvadi6@gmail.com>
2024-08-28 00:41:26 +0530

    till comparison operator

--- 27-08-2024/index.js
+++ 27-08-2024/index.js
@@ -1,6 +1,23 @@
-let num=5;
-console.log(num);
-let newNum = num++;
-console.log(newNum);
-newNum = ++num;
-console.log(newNum);
+let firstName = 'Gaurang ';
+let lastName = 'Dalvadi';
+let fLength = firstName.length;
+let fullName= `my fullName is ${firstName+lastName}`;
+console.log(fullName)
+// alert(fullName);
+
+let firstChar = firstName.charAt(0);
+let lastChar = firstName.charAt(firstName.length-1);
+let emptyString = "";
+let spaceString = " ";
+
+
+// let name='my name is "Gaurang" ';
+// let char='a';
+// let num="123"
+
+// let num=5;
+// console.log(num);
+// let newNum = num++;
+// console.log(newNum);
+// newNum = ++num;
+// console.log(newNum);
