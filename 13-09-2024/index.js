72b8f6ecc1e89274be7650ae58cfb2c5697c51a8
gaurangdalvadi6 <gaurangdalvadi6@gmail.com>
2024-09-13 22:47:25 +0530

    random number generator

---
+++ 13-09-2024/index.js
@@ -0,0 +1,70 @@
+var name1= prompt("Enter name1 :");
+var name2 = prompt("Enter name2 :");
+var calculate = Math.floor((Math.random()*100))+1;
+alert(calculate);
+
+// var n = Math.random();
+// n=n*6;
+// n = Math.floor(n)+1;
+// console.log(n);
+
+// function bmiCalculator(weight,height){
+//     var bmi = weight/Math.pow(height,2);
+//     return Math.round(bmi);
+// }
+// console.log(bmiCalculator(65,1.8));
+
+// function getMilk(money,costPerBottle) {   
+//     console.log("leaveHouse");
+//     console.log("moveRight");
+//     console.log("moveRight");
+//     console.log("moveUp");
+//     console.log("moveUp");
+//     console.log("moveUp");
+//     console.log("moveUp");
+//     console.log("moveRight");
+//     console.log("moveRight");
+//     console.log("buy "+calcBottles(money,costPerBottle)+" bottles of milk");
+//     console.log("moveLeft");
+//     console.log("moveLeft");
+//     console.log("moveDown");
+//     console.log("moveDown");
+//     console.log("moveDown");
+//     console.log("moveDown");
+//     console.log("moveLeft");
+//     console.log("moveLeft");
+//     console.log("enterHouse");
+//     return calcChange(money,costPerBottle);
+//   }
+
+// function calcBottles(startingMoney,costPerBottle){
+//     var numberOfBottles = Math.floor(startingMoney/costPerBottle);
+//     return numberOfBottles;
+// }
+
+// function calcChange(startingMoney,costPerBottle){
+//     var change = startingMoney%costPerBottle;
+//     return change;
+// }
+
+// console.log(getMilk(5,2));
+
+// let name1 = prompt("Enter Your name : ");
+
+// alert("Hello "+name1.charAt(0).toUpperCase()+name1.slice(1).toLowerCase());
+
+
+// let name1 = "Gaurang";
+// let newName = name1.toUpperCase();
+// console.log(newName);
+// alert(prompt("Enter any pragraph").slice(0,140));
+
+// let name1 = "Gaurang";
+// console.log(name1.slice(0,2));
+// let name2 =name1.slice(1,2);
+// console.log(name2);
+
+// let input = prompt("Enter anything?");
+// let totalInputNumber=input.length;
+// let leftNumber = 140-totalInputNumber;
+// alert("Your input number is "+totalInputNumber+" & left character is "+leftNumber); 
