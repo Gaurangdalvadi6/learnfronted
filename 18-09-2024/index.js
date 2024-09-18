07d063d37a0e010216c0822a3e1d34fb4d55cbba
gaurangdalvadi6 <gaurangdalvadi6@gmail.com>
2024-09-18 20:35:47 +0530

    till simon game step-7

---
+++ 18-09-2024/index.js
@@ -0,0 +1,48 @@
+$("button").click(function(){
+    $("h1").animate({
+        opacity:0.5
+    });
+});
+
+// $("button").click(function(){
+//     $("h1").slideToggle();
+// });
+
+// $("button").click(function(){
+//     $("h1").fadeToggle();
+// });
+
+// $("button").click(function(){
+//     $("h1").toggle();
+// });
+
+// $(document).keypress(function(event){
+//     $("h1").text(event.key);
+// });
+
+// $("input").keypress(function(event){
+//     console.log(event.key);
+// });
+
+// $("button").click(function(){
+//     $("h1").css("color","purple");
+// });
+
+
+// $("h1").click(function(){
+//     $("h1").css("color","purple");
+// });
+
+// $("img").attr("src");
+// $("a").attr("href","https://bing.com");
+
+// $("h1").text("Good Bye");
+// $("button").html("<em>Abhi</em>");
+
+// $("h1").addClass("big-title margin-50");
+
+// for(var i=0;i<5;i++){
+//     document.querySelectorAll("button")[i].addEventListener("click",function(){
+//         document.querySelector("h1").style.color="purple";
+//     });
+// }
