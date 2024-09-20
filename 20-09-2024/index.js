4b19f8ac7f0a6d5b85a8330e4efc24ccf2ac323a
gaurangdalvadi6 <gaurangdalvadi6@gmail.com>
2024-09-20 22:43:09 +0530

    till video of technical thapa

---
+++ 20-09-2024/index.js
@@ -0,0 +1,8 @@
+console.log("Hello my name is gaurang and i am java developer");
+var num = 5;
+num = 55;
+//@ts-ignore
+var sum = function (a, b) {
+    return a + b;
+};
+console.log(sum(5, 4));
---
+++ 20-09-2024/index.ts
@@ -0,0 +1,9 @@
+console.log("Hello my name is gaurang and i am java developer");
+var num:number=5;
+num=55;
+
+//@ts-ignore
+const sum = (a:number,b:number):number =>{
+    return a+b;
+}
+console.log(sum(5,4));
