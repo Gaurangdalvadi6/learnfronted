6046c526550b062b06857e865bf6c95f04de875e
gaurangdalvadi6 <gaurangdalvadi6@gmail.com>
2024-09-23 23:01:00 +0530

    javascript part 3 completed

---
+++ 23-09-2024/app.js
@@ -0,0 +1,34 @@
+// var nums = [[1,2],[3,4],[4,5]];
+// var nums = [[2,3],[3,4],[4,5]];
+
+// let tictak=[["x",null,"0"],[null,"x",null],["0",null,"x"]];
+
+
+// let firstN = [7,9,0,-2];
+// let n=3;
+// let newarr= firstN.slice(0,n);
+
+// let lastN = [7,9,0,-2];
+// let start=1;
+// let end=3;
+// let newLastN = lastN.slice(start,end+1);
+
+// let str= "";
+// if (str.length ===0 ) {
+//     console.log("empty");
+// }else{
+//     console.log("not empty");
+// }
+
+// let str ="ApNaCoLlEge";
+// let idx=1;
+// if (str[idx]==str[idx].toLowerCase()) {
+//     console.log("character is lowercase");
+// } else {
+//     console.log("character is uppercase");
+// }
+
+
+let str = prompt("enter any string");
+console.log(`original string = ${str}`);
+console.log(`stringwithoutspaces=${str.trim()}`);
