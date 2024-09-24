b3b5ea1c28e3a723edeebb480d8da8da05550148
gaurangdalvadi6 <gaurangdalvadi6@gmail.com>
2024-09-24 21:45:29 +0530

    for of loop

---
+++ 24-09-2024/index.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let heroes = [["superman","batman","wonder woman"],["spiderman","ironman","thor"]];
+
+// for (list of heroes) {
+//     for (hero of list) {
+//         console.log(hero);
+//     }
+// }
+
+// let fruits =["apple","banana","pineapple","strawberry"];
+
+// for (fruit of fruits) {
+//     console.log(fruit);
+// }
