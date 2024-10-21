63db39725e2abe1dd1b6f6e1e4e607b6a13d4d20
gaurangdalvadi6 <gaurangdalvadi6@gmail.com>
2024-10-21 22:41:57 +0530

    till getAttribute ans setAttribute

--- 21-10-2024/index.js
+++ 21-10-2024/index.js
@@ -1,24 +1,84 @@
-let btn = document.querySelector("button");
+let div = document.querySelector("div");
+console.log(div);
 
-btn.addEventListener("click",function(){
-    let h3 = document.querySelector("h3");
-    let randomColor = getRandomColor();
-    h3.innerHTML=randomColor;
+let id = div.getAttribute("id");
+console.log(id);
 
-    let div = document.querySelector("div");
-    div.style.backgroundColor=randomColor;
+let par = document.querySelector("p");
+console.log(par);
 
-    console.log("color updated");
-});
+let classname = par.getAttribute("class");
+console.log(classname);
 
-function getRandomColor(){
-    let red = Math.floor(Math.random()*256);
-    let green = Math.floor(Math.random()*256);
-    let blue = Math.floor(Math.random()*256);
+par.setAttribute("class","gaurang");
+console.log(par);
 
-    let color = `rgb(${red},${green},${blue})`;
-    return color;
-}
+// let divs = document.querySelectorAll(".box");
+
+// let idx = 1;
+// for (div of divs) {
+//     div.innerText=`new unique value ${idx}`;
+//     idx++;
+// }
+
+// let heading = document.querySelector("h2");
+// heading.innerText = heading.innerText + " from Apna College students";
+
+// heading.append("from Apna College students");
+
+// let div = document.querySelector("div");
+// console.dir(div);
+
+// let heading = document.querySelector("h3");
+
+
+// let btn = document.querySelector("button");
+
+// btn.addEventListener("click",function(event){
+//     console.log(event);
+//     // console.log(this);
+//     this.style.backgroundColor="blue";
+// });
+
+// btn.addEventListener("dblclick",function(event){
+//     console.log(event);
+//     console.log("button clicked");
+// })
+
+
+// let p = document.querySelector("p");
+
+// p.addEventListener("click",()=>{
+//     console.log("clicked on paragraph")
+// });
+
+// let box = document.querySelector(".box");
+// box.addEventListener("mouseenter",()=>{
+//     console.log("mouse inside div");
+// });
+
+// random color generator code
+// let btn = document.querySelector("button");
+
+// btn.addEventListener("click",function(){
+//     let h3 = document.querySelector("h3");
+//     let randomColor = getRandomColor();
+//     h3.innerHTML=randomColor;
+
+//     let div = document.querySelector("div");
+//     div.style.backgroundColor=randomColor;
+
+//     console.log("color updated");
+// });
+
+// function getRandomColor(){
+//     let red = Math.floor(Math.random()*256);
+//     let green = Math.floor(Math.random()*256);
+//     let blue = Math.floor(Math.random()*256);
+
+//     let color = `rgb(${red},${green},${blue})`;
+//     return color;
+// }
 
 
 
