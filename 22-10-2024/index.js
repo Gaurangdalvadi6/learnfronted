d254b9d9a6c2ccbb12bd6b8a3b23edbda033ad03
gaurangdalvadi6 <gaurangdalvadi6@gmail.com>
2024-10-22 22:52:22 +0530

    till event

--- 22-10-2024/index.js
+++ 22-10-2024/index.js
@@ -1,12 +1,39 @@
-let btn = document.createElement("button");
-btn.innerText="Click Me!";
-btn.style.backgroundColor="red";
-btn.style.color="white";
+let btn1 = document.querySelector("#btn1");
 
-document.querySelector("body").prepend(btn);
+btn1.addEventListener("click",(e)=>{
+    console.log("button was clicked");
+    console.log(e);
+    console.log(e.type);
+    console.log(e.target);
+});
 
-let para = document.querySelector("p");
-para.classList.add("newClass");
+// btn1.onclick = (e)=>{
+//     console.log(e);
+//     console.log(e.type);
+//     console.log(e.target);
+//     // console.log("button was clicked");
+//     // let a = 25;
+//     // a++;
+//     // console.log(a);
+// }
+
+// let div = document.querySelector("div");
+// div.onmouseover = (e)=>{
+//     console.log("mouse inside a div");
+//     console.log(e);
+//     console.log(e.type);
+//     console.log(e.target);
+// }
+
+// let btn = document.createElement("button");
+// btn.innerText="Click Me!";
+// btn.style.backgroundColor="red";
+// btn.style.color="white";
+
+// document.querySelector("body").prepend(btn);
+
+// let para = document.querySelector("p");
+// para.classList.add("newClass");
 
 // let newbtn = document.createElement("button");
 // newbtn.innerText="click me!";
